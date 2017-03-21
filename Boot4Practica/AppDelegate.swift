fbfa0c8e155169b4f647855b8034997e67a07883
byjuanamn <juan.antonio.martin@gmail.com>
2017-03-21 23:36:11 +0100

    Codigo minino para acceder a la cuenta y  sus contamines

--- Boot4Practica/AppDelegate.swift
+++ Boot4Practica/AppDelegate.swift
@@ -17,10 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let azsClient = AZSCloudBlobClient()
-
-        
-        
         return true
     }
 
--- Boot4Practica/ViewController.swift
+++ Boot4Practica/ViewController.swift
@@ -10,16 +10,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var acount: AZSCloudStorageAccount!
+    var blobClient: AZSCloudBlobClient!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupAzureStorageConnect()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setupAzureStorageConnect() {
+        let credetials = AZSStorageCredentials(accountName: "juanboot4", accountKey: "4GrSb/HgrXwXBxWhpe8SzZkqdyDpUERY4kzZfE93Ud1Kea168R6GVyOOK0tIH9CvjnSkcgJp4wRkMRUpjBhilQ==")
+        do {
+            acount = try AZSCloudStorageAccount(credentials: credetials, useHttps: true)
+            blobClient = acount.getBlobClient()
+            readAllContainers()
+            
+        } catch let error {
+            print("\(error.localizedDescription)")
+        }
+    }
+    
+    fileprivate func readAllContainers() {
+        blobClient.listContainersSegmented(with: nil,
+                                           prefix: nil,
+                                           containerListingDetails: AZSContainerListingDetails.all,
+                                           maxResults: -1,
+                                           completionHandler: { (error, containersResults) in
+                                            
+                                            if let _ = error {
+                                                print("\(error?.localizedDescription)")
+                                                return
+                                            }
+                                            
+                                            for item in (containersResults?.results)! {
+                                                print(item)
+                                            }
+        
+        
+        })
+
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
